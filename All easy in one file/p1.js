0c49329cf7c6e2b70bcae315774c6fdb8036062c
abhinash1051 <abhinashkumar03619@gmail.com>
2026-05-02 23:55:34 +0530

    nqt

---
+++ All easy in one file/p1.js
@@ -0,0 +1,11 @@
+function guessNumber() {
+
+    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    console.log(a == b);
+    console.log(a === b);
+
+
+}
+guessNumber();
